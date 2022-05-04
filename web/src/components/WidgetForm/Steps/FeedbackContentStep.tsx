5ee389cb96690dd19adc8e5fc063c22ed115b737
luisdasilvahenrique <luisenriklh1177027@gmail.com>
2022-05-04 10:51:13 -0300

    Steps: Feedback-content-sucess-step

---
+++ web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -0,0 +1,15 @@
+import { CloseButtom } from "../../CloseButtom";
+
+export function FeedbackContentStep() {
+  return (
+    <>
+      <header>
+        <span className="text-xl leading-6">Deixe seu feedback</span>
+
+        <CloseButtom />
+      </header>
+
+      <div className="flex py-8 gap-2 w-full"></div>
+    </>
+  );
+}
