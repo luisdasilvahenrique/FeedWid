2f53850207d9bcb9f6b14c05db1273ce3f03229d
luisdasilvahenrique <luisenriklh1177027@gmail.com>
2022-05-04 15:05:45 -0300

    function: back in arrow button

--- web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -1,15 +1,39 @@
+import { ArrowLeft } from "phosphor-react";
+import { FeedbackType, feedbackTypes } from "..";
 import { CloseButtom } from "../../CloseButtom";
 
-export function FeedbackContentStep() {
+interface FeedbackContentStepProps {
+  feedbackType: FeedbackType;
+  onFeedbackRestartRequested: () => void;
+}
+
+export function FeedbackContentStep({ feedbackType, 
+onFeedbackRestartRequested }: FeedbackContentStepProps) {
+  
+  const feedbackTypeInfo = feedbackTypes[feedbackType];
+
   return (
     <>
       <header>
-        <span className="text-xl leading-6">Deixe seu feedback</span>
 
+        <button 
+        type="button" 
+        className="w-4 h-4 top-5 left-5 absolute text-zinc-400 hover:text-zinc-100"
+        onClick={onFeedbackRestartRequested}>
+          <ArrowLeft weight="bold"    />
+        </button>
+
+        <span className="text-xl leading-6 flex items-center gap-2">
+          <img src={feedbackTypeInfo.image.source} alt={feedbackTypeInfo.image.alt} className="w-6 h-6" />
+          {feedbackTypeInfo.title}
+        </span>
+          
         <CloseButtom />
       </header>
 
-      <div className="flex py-8 gap-2 w-full"></div>
+      <div className="flex py-8 gap-2 w-full">
+          
+      </div>
     </>
   );
 }
