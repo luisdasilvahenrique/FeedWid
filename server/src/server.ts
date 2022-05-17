113d73da898bd4185abdb513174e12dc47030f93
luisdasilvahenrique <luisenriklh1177027@gmail.com>
2022-05-16 22:12:55 -0300

    install: cors for back-end

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,10 @@
 import  express  from "express";
+import cors from "cors"
 import { routes } from "./routes";
 
 const app = express();
 
+app.use(cors())//segurança de back-end contra front-end inadequados não o consumam
 app.use(express.json());
 app.use(routes);
 
